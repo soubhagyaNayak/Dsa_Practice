e30291651ea971be1fd7fbe9a5d7e9a138747f8d
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2023-09-16 08:50:05 +0530

    Dsa_Practice

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,ans=0;
+    cin>>a;
+    while(a!=0){
+        b=a%10;
+        ans=ans*10+b;
+        a=a/10;
+    }
+    cout<<ans;
+    return 0;
+}
